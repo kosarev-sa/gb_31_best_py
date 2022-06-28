6a8640ee9c35855db71786967007366088661398
Shirguin <shirguin@yandex.ru>
2022-06-28 07:20:44 +0300

    added recommended vacancies

--- BestJob/vacancies/views.py
+++ BestJob/vacancies/views.py
@@ -221,8 +221,14 @@ class RecommendedVacancyList(ListView, BaseClassContextMixin):
         cv = CV.objects.get(id=self.kwargs['pk'])
         context = {
             'vacancies': Vacancy.objects.filter(specialization=cv.speciality),
+            'title': "Рекомендованные вакансии",
+            'heading': "Рекомендованные вакансии",
+
+            # Сделать переход в шапке, куда?
+            # 'link': "",
+            # 'heading_link': "",
         }
-        
+
         return self.render_to_response(context)
         
 
