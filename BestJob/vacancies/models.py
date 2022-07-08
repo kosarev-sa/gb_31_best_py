121f603a4d20d6066f3abf271c8fb64d0f0f0410
Okr <kr.oleg@gmail.com>
2022-07-08 23:05:21 +0300

    Delete max size for TextField

--- BestJob/vacancies/models.py
+++ BestJob/vacancies/models.py
@@ -21,13 +21,13 @@ class Vacancy(models.Model):
     name = models.CharField(max_length=256, blank=True, verbose_name='Название вакансии')
     experience = models.CharField(max_length=3, choices=EXPERIENCE, blank=True, verbose_name='Опыт')
     city = models.CharField(max_length=20, blank=True, verbose_name='Вакансия в городе')
-    description = models.TextField(max_length=5000, blank=True, null=True, verbose_name='Описание вакансии')
+    description = models.TextField(blank=True, null=True, verbose_name='Описание вакансии')
     salary_from = models.DecimalField(max_digits=8, decimal_places=1, blank=True, null=True, verbose_name='доход от')
     salary_to = models.DecimalField(max_digits=8, decimal_places=1, blank=True, null=True, verbose_name='доход до')
     currency = models.PositiveSmallIntegerField(choices=Currency.choices, default=Currency.RUB, verbose_name='валюта')
     salary_on_hand = models.BooleanField(default=True, blank=True, verbose_name='Зарплата на руки')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
-    moderators_comment = models.TextField(max_length=5000, blank=True, null=True)
+    moderators_comment = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.name
