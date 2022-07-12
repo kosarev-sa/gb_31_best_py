f57d87b82868684ec96ac4101de03d06b5211728
Okr <kr.oleg@gmail.com>
2022-07-12 12:08:51 +0300

    Fix footer if not data

--- BestJob/users/context_processors.py
+++ BestJob/users/context_processors.py
@@ -6,11 +6,15 @@ from users.models import WorkerProfile, EmployerProfile
 
 def footer_lists(request):
     footer_employer_list = EmployerProfile.objects.all()
-    footer_employer_list = random.sample(list(footer_employer_list), 6)
-
-    return {"footer_list_1": footer_employer_list[0:3],
-            "footer_list_2": footer_employer_list[3:]
-            }
+    if footer_employer_list:
+        footer_employer_list = random.sample(list(footer_employer_list), 6)
+        return {"footer_list_1": footer_employer_list[0:3],
+                "footer_list_2": footer_employer_list[3:]
+                }
+    else:
+        return {"footer_list_1": None,
+                "footer_list_2": None
+                }
 
 
 def roles(request):
