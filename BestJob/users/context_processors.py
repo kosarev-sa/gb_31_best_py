34453fbe66194cf18eea9dbd0c4402305a9997d6
Okr <kr.oleg@gmail.com>
2022-07-13 12:08:38 +0300

    Edit footer data

--- BestJob/users/context_processors.py
+++ BestJob/users/context_processors.py
@@ -7,21 +7,17 @@ from users.models import WorkerProfile, EmployerProfile
 def footer_lists(request):
     footer_employer_list = EmployerProfile.objects.all().exclude(
                 status__status="NPB").exclude(status__status="RJC").exclude(image='')
-    if footer_employer_list:
-        footer_employer_list = random.sample(list(footer_employer_list), 6)
 
+    footer_data = {"footer_list_1": None, "footer_list_2": None }
+
+    if footer_employer_list:
         if len(footer_employer_list) >= 6:
-            return {"footer_list_1": footer_employer_list[0:3],
-                    "footer_list_2": footer_employer_list[3:]
-                    }
-        else:
-            return {"footer_list_1": None,
-                    "footer_list_2": None
-                    }
-    else:
-        return {"footer_list_1": None,
-                "footer_list_2": None
-                }
+            footer_employer_list = random.sample(list(footer_employer_list), 6)
+            footer_data["footer_list_1"] = footer_employer_list[0:3]
+            footer_data["footer_list_2"] = footer_employer_list[3:]
+
+
+    return footer_data
 
 
 def roles(request):
